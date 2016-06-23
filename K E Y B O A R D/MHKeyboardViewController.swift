d5f5674336a4a4c256ded0d181e23c79e1469fe6
Michael Hulet <bestraysfan@me.com>
2016-06-22 21:36:48 -0500

    Saving my work before I operate on my laptop

--- K E Y B O A R D/MHKeyboardViewController.swift
+++ K E Y B O A R D/MHKeyboardViewController.swift
@@ -43,7 +43,12 @@ class MHKeyboardViewController: UIInputViewController{
         let darkGreen = UIColor(r: 90, g: 183, b: 160)
         view.setBackground(gradient: verticalGradient(view.frame, colors: [darkGreen, view.backgroundColor!, darkGreen]))
         eachKey{(key: UIButton) in
-            key.setBackground(gradient: verticalGradient(key.frame, top: key.backgroundColor!, bottom: UIColor(r: 253, g: 130, b: 159)))
+            if key.titleLabel?.text == "^"{
+                key.setBackground(gradient: verticalGradient(key.frame, colors: [UIColor(r: 183, g: 119, b: 240), UIColor(r: 123, g: 85, b: 205)]))
+            }
+            else{
+                key.setBackground(gradient: verticalGradient(key.frame, top: UIColor(r: 253, g: 130, b: 159), bottom: key.backgroundColor!))
+            }
         }
     }
 
@@ -65,12 +70,16 @@ class MHKeyboardViewController: UIInputViewController{
 
     func eachKey(closure: (key: UIButton) -> Void) -> Void{
         for sub in view.subviews{
-            for button in sub.subviews{
-                guard let button = button as? UIButton else{
-                    continue
+            guard let button = sub as? UIButton else{
+                for button in sub.subviews{
+                    guard let button = button as? UIButton else{
+                        continue
+                    }
+                    closure(key: button)
                 }
-                closure(key: button)
+                continue
             }
+            closure(key: button)
         }
     }
 
@@ -88,19 +97,29 @@ class MHKeyboardViewController: UIInputViewController{
             textDocumentProxy.deleteBackward()
         }
         key.backgroundColor = UIColor(r: 216, g: 40, b: 130)
-        reverseGradient(key: key)
+        invertGradient(key: key)
     }
 
     @objc func cleanUpPress(key: UIButton) -> Void{
         key.backgroundColor = UIColor(r: 250, g: 48, b: 129)
-        reverseGradient(key: key)
+        invertGradient(key: key)
     }
 
-    func reverseGradient(key key: UIButton) -> Void{
+    func invertGradient(key key: UIButton) -> Void{
         guard let gradient = key.layer.sublayers?[0] as? CAGradientLayer else{
             return
         }
-        gradient.colors = gradient.colors?.reverse()
+        if gradient.colors!.count % 2 == 0{
+            gradient.colors = gradient.colors?.reverse()
+        }
+        else{
+            var new = [AnyObject]()
+            for i in 0..<gradient.colors!.count - 1{
+                new.append(gradient.colors![i + 1])
+            }
+            new.append(gradient.colors![gradient.colors!.count - 2])
+            gradient.colors = new
+        }
     }
 }
 
