6cd9d6451446176687896c986486ffa13ae469f2
Michael Hulet <bestraysfan@me.com>
2016-06-24 06:26:12 -0500

    ＷＩＲＥＤ    ＵＰ    ＴＨＥ    ＵＩ

--- K E Y B O A R D/MHKeyboardViewController.swift
+++ K E Y B O A R D/MHKeyboardViewController.swift
@@ -12,6 +12,7 @@ enum MHKeyCase{
     case Capital
     case Lower
     case Punctuation
+    case Whitespace
     var layout: [[String]]{
         get{
             switch self{
@@ -21,7 +22,20 @@ enum MHKeyCase{
                     return [["１", "２", "３", "４", "５", "６", "７", "８", "９", "０"], ["ｑ", "ｗ", "ｅ", "ｒ", "ｔ", "ｙ", "ｕ", "ｉ", "ｏ", "ｐ"], ["ａ", "ｓ", "ｄ", "ｆ", "ｇ", "ｈ", "ｊ", "ｋ", "ｌ"], ["ｚ", "ｘ", "ｃ", "ｖ", "ｂ", "ｎ", "ｍ"], ["＊", "ｓｐａｃｅ"]]
                 case .Punctuation:
                     return [["－", "／", "：", "；", "（", "）", "＄", "＆", "＠", "＂"], ["［", "］", "｛", "｝", "＃", "％", "＾", "＊", "＋", "＝"], ["＿", "＼", "｜", "～", "＜", "＞", "€", "￡", "￥"], ["．", "，", "？", "！", "＇", "｟", "｠"], ["Ａ", "ＳＰＡＣＥ"]]
+                default:
+                    return []
             }
+        }
+    }
+    var characters: [Character]{
+        get{
+            var all = [Character]()
+            for row in self.layout{
+                all += row.map({(str: String) -> Character in
+                    return Character(str)
+                })
+            }
+            return all
         }
     }
 }
@@ -33,44 +47,53 @@ class MHKeyboardViewController: UIInputViewController{
     @IBOutlet weak var backspaceKey: UIButton!
     @IBOutlet weak var shiftKey: UIButton!
     @IBOutlet weak var alternateLayoutKey: UIButton!
-    var currentCase: MHKeyCase!{
+    @IBOutlet weak var returnKey: UIButton!
+    var currentCase: MHKeyCase = .Capital{
         didSet{
-            eachKey({(key: UIButton, row: Int, index: Int) in
-                var updatedIndex = index
-                if row == 3{
-                    updatedIndex = index - 1
+            eachKey({(key: UIButton) in
+                let keyPosition = self.indexOf(key: key)
+                var updatedIndex = keyPosition.index
+                if keyPosition.row == 3{
+                    updatedIndex = keyPosition.index - 1
                 }
-                else if row == 4 && index == 2{
+                else if keyPosition.row == 4 && keyPosition.index == 2{
                     updatedIndex = 1
                 }
-                key.setTitle(self.currentCase.layout[row][updatedIndex], forState: .Normal)
+                key.setTitle(self.currentCase.layout[keyPosition.row][updatedIndex], forState: .Normal)
             }, filter: {(key: UIButton) -> Bool in
                     return key.titleLabel?.text != nil
             })
+            if currentCase == .Punctuation{
+                shiftKey.userInteractionEnabled = false
+            }
+            else{
+                shiftKey.userInteractionEnabled = true
+            }
         }
     }
-
-    override func updateViewConstraints() -> Void{
-        super.updateViewConstraints()
-        // Add custom view sizing constraints here
-    }
+    var capsLock = false
 
     override func viewDidLoad() -> Void{
         super.viewDidLoad()
-
         view = UINib(nibName: "K E Y B O A R D   V I E W", bundle: nil).instantiateWithOwner(self, options: nil)[0] as! UIView
-        nextKeyboardButton.addTarget(self, action: #selector(self.advanceToNextInputMode), forControlEvents: .TouchUpInside)
+        nextKeyboardButton.addTarget(self, action: #selector(switchToNextKeyboard), forControlEvents: .TouchUpInside)
+        let capsLockTrigger = UITapGestureRecognizer()
+        capsLockTrigger.numberOfTapsRequired = 2
+        capsLockTrigger.addTarget(self, action: #selector(lockCaps))
+        shiftKey.addGestureRecognizer(capsLockTrigger)
     }
 
     override func viewWillAppear(animated: Bool) -> Void{
         super.viewWillAppear(animated)
         let darkGreen = UIColor(r: 90, g: 183, b: 160)
         view.setBackground(gradient: verticalGradient(view.frame, colors: [darkGreen, view.backgroundColor!, darkGreen]))
-        eachKey({(key: UIButton, _, _) in
+        eachKey({(key: UIButton) in
             key.layer.cornerRadius = 10
-            key.addTarget(self, action: #selector(self.pressed(_:)), forControlEvents: .TouchDown)
-            key.addTarget(self, action: #selector(self.cleanUpPress(_:)), forControlEvents: .TouchUpInside)
-            key.addTarget(self, action: #selector(self.cleanUpPress(_:)), forControlEvents: .TouchCancel)
+            if key != self.nextKeyboardButton{
+                key.addTarget(self, action: #selector(self.pressed(_:)), forControlEvents: .TouchDown)
+                key.addTarget(self, action: #selector(self.cleanUpPress(_:)), forControlEvents: .TouchUpInside)
+                key.addTarget(self, action: #selector(self.cleanUpPress(_:)), forControlEvents: .TouchCancel)
+            }
             key.imageView?.contentMode = .ScaleAspectFit
             if self.specialKeys.contains(key){
                 key.setBackground(gradient: verticalGradient(key.frame, colors: [UIColor(r: 183, g: 119, b: 240), UIColor(r: 123, g: 85, b: 205)]))
@@ -88,7 +111,7 @@ class MHKeyboardViewController: UIInputViewController{
         }
         greenGradient.frame = view.frame
         view.setBackground(gradient: greenGradient)
-        eachKey({(key: UIButton, _, _) in
+        eachKey({(key: UIButton) in
             guard let pinkGradient = key.layer.sublayers![0] as? CAGradientLayer else{
                 return
             }
@@ -105,10 +128,9 @@ class MHKeyboardViewController: UIInputViewController{
                 key.imageEdgeInsets = UIEdgeInsets(top: 0, left: factor, bottom: 0, right: factor)
             }
         })
-        self.currentCase = .Lower
     }
 
-    func eachKey(closure: (key: UIButton, row: Int, index: Int) -> Void, filter: (key: UIButton) -> Bool = {(_) in return true}) -> Void{
+    func eachKey(closure: (key: UIButton) -> Void, filter: (key: UIButton) -> Bool = {(_) in return true}) -> Void{
         var applicableKeys: [UIButton] = []
         for row in view.subviews{
             for current in row.subviews{
@@ -122,13 +144,36 @@ class MHKeyboardViewController: UIInputViewController{
             }
         }
         for key in applicableKeys{
-            closure(key: key, row: key.tag / 10, index: key.tag % 10)
+            closure(key: key)
         }
+    }
+    func indexOf(key key: UIButton) -> (row: Int, index: Int){
+        return (row: key.tag / 10, index: key.tag % 10)
     }
 
     @objc func pressed(key: UIButton) -> Void{
-        if key.titleLabel?.text?.length != 3 && key.imageView?.image == nil{
-            textDocumentProxy.insertText(key.titleLabel!.text! == "ＳＰＡＣＥ" ? "   " : key.titleLabel!.text!)
+        if !(capsLock && key == shiftKey){
+            invertGradient(key: key)
+        }
+        if key == shiftKey && !capsLock && currentCase != .Punctuation{
+            currentCase = currentCase == .Capital ? .Lower : .Capital
+            if currentCase == .Capital{
+                shiftKey.setImage(UIImage(named: "Shift-Caps")!, forState: .Normal)
+            }
+            else{
+                shiftKey.setImage(UIImage(named: "Shift-Lower")!, forState: .Normal)
+            }
+        }
+        else if key == shiftKey && capsLock && currentCase != .Punctuation{
+            shiftKey.setImage(UIImage(named: "Shift-Lower")!, forState: .Normal)
+            currentCase = .Lower
+            capsLock = false
+        }
+        else if key == alternateLayoutKey{
+            currentCase = currentCase == .Punctuation ? .Capital : .Punctuation
+        }
+        else if key.titleLabel?.text == "ＳＰＡＣＥ" || key.titleLabel?.text == "ｓｐａｃｅ"{
+            textDocumentProxy.insertText("   ")
         }
         else if key == backspaceKey{
             guard textDocumentProxy.documentContextBeforeInput?.characters.last != " " else{
@@ -139,13 +184,33 @@ class MHKeyboardViewController: UIInputViewController{
             }
             textDocumentProxy.deleteBackward()
         }
-        key.backgroundColor = UIColor(r: 216, g: 40, b: 130)
-        invertGradient(key: key)
+        else if key == returnKey{
+            textDocumentProxy.insertText("\n")
+        }
+        else{
+            textDocumentProxy.insertText(key.titleLabel!.text!)
+        }
     }
 
     @objc func cleanUpPress(key: UIButton) -> Void{
-        key.backgroundColor = UIColor(r: 250, g: 48, b: 129)
-        invertGradient(key: key)
+        if !(key == shiftKey && capsLock){
+            invertGradient(key: key)
+        }
+        if key != shiftKey && currentCase == .Capital && !capsLock{
+            currentCase = .Lower
+        }
+    }
+
+    @objc func lockCaps() -> Void{
+        invertGradient(key: shiftKey)
+        shiftKey.setImage(UIImage(named: "Shift-Caps-Lock")!, forState: .Normal)
+        currentCase = .Capital
+        capsLock = true
+    }
+
+    @objc func switchToNextKeyboard() -> Void{
+        invertGradient(key: nextKeyboardButton)
+        advanceToNextInputMode()
     }
 
     func invertGradient(key key: UIButton) -> Void{
